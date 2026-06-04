9b5d27957ca572c95fbe1ea377a5a21dc9290f15
ThandoLove <tiampofu62@gmail.com>
2026-06-04 08:45:22 -0700

    Update

---
+++ OperationalWorkspaceApplication/Interfaces/IAttachmentProvider.cs
@@ -0,0 +1,15 @@
+﻿using OperationalWorkspaceApplication.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperationalWorkspaceApplication.Interfaces
+{
+    public interface IAttachmentProvider
+    {
+        Task<List<AttachmentDto>> GetAttachmentsAsync(
+            string ownerType,
+            string ownerId,
+            CancellationToken cancellationToken);
+    }
+}
